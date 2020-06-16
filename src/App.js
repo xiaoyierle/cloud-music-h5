60a76e5b9c64c3c37a57e9557e30c9568409596c
xiaoyierle <384650553@qq.com>
2020-06-16 14:43:09 +0800

    fix 更新推荐列表页

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react'
-import { IconStyle } from './assets/iconfont/iconfont'
+import { Provider } from 'react-redux'
 import { GlobalStyle } from './style'
+import { renderRoutes } from 'react-router-config' //renderRoutes 读取路由配置转化为 Route 标签
+import { IconStyle } from './assets/iconfont/iconfont'
+import store from './store/index'
+import routes from './routes/index.js'
+import { HashRouter } from 'react-router-dom'
 
 class App extends Component {
   render() {
+    console.log(routes)
     return (
-      <div className="App">
-        <GlobalStyle></GlobalStyle>
-        <IconStyle></IconStyle>
-        <i className="iconfont">&#xe62b;</i>
-      </div>
+      <Provider store={store}>
+        <HashRouter>
+          <GlobalStyle></GlobalStyle>
+          <IconStyle></IconStyle>
+          { renderRoutes (routes) }
+        </HashRouter>
+      </Provider>
     )
   }
 }
 
-export default App;
+export default App
--- src/assets/iconfont/iconfont.js
+++ src/assets/iconfont/iconfont.js
@@ -3,12 +3,12 @@ import {createGlobalStyle} from 'styled-components';
 export const IconStyle = createGlobalStyle`
 @font-face {
   font-family: 'iconfont';  /* project id 1887241 */
-  src: url('//at.alicdn.com/t/font_1887241_gy3rlkg7qna.eot');
-  src: url('//at.alicdn.com/t/font_1887241_gy3rlkg7qna.eot?#iefix') format('embedded-opentype'),
-  url('//at.alicdn.com/t/font_1887241_gy3rlkg7qna.woff2') format('woff2'),
-  url('//at.alicdn.com/t/font_1887241_gy3rlkg7qna.woff') format('woff'),
-  url('//at.alicdn.com/t/font_1887241_gy3rlkg7qna.ttf') format('truetype'),
-  url('//at.alicdn.com/t/font_1887241_gy3rlkg7qna.svg#iconfont') format('svg');
+  src: url('//at.alicdn.com/t/font_1887241_ugenj492l9i.eot');
+  src: url('//at.alicdn.com/t/font_1887241_ugenj492l9i.eot?#iefix') format('embedded-opentype'),
+  url('//at.alicdn.com/t/font_1887241_ugenj492l9i.woff2') format('woff2'),
+  url('//at.alicdn.com/t/font_1887241_ugenj492l9i.woff') format('woff'),
+  url('//at.alicdn.com/t/font_1887241_ugenj492l9i.ttf') format('truetype'),
+  url('//at.alicdn.com/t/font_1887241_ugenj492l9i.svg#iconfont') format('svg');
 }
 
 .iconfont {
